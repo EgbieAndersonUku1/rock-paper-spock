dd1e059ed4008a2631a69f18441527e13ea4d683
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2024-07-31 15:18:43 +0100

    feat: Implement Version 2 with Enhanced Error Handling
    - Added validatePlayerInput function to ensure both player inputs are valid choices.
    - Updated getWinner function to simplify logic, relying on rpsls function to handle invalid choices. Now, if player 1 is not the winner, player 2 wins.
    - Updated example usage to include error handling for invalid inputs.
    
    The goal of this update iss to provide better reliability and user experience by validating inputs before determining the game outcome,
      preventing incorrect results from invalid entries.

--- lizard-spock-v2.js
+++ lizard-spock-v2.js
@@ -1,4 +1,6 @@
 function rpsls(pl1,pl2){
+    validatePlayerInput(pl1, "player 1");
+    validatePlayerInput(pl2, "player 2")
     return getWinner(pl1, pl2)
 }
 
@@ -7,7 +9,7 @@ function getWinner(p1, p2) {
     if (p1 === p2) {
         return "Draw!";
     }
-    return isWinningCombination(p1, p2) ? "Player 1 Won!" : isWinningCombination(p2, p1) ? "Player 2 Won!" : "Error";
+    return isWinningCombination(p1, p2) ? "Player 1 Won!" : "Player 2 Won!";
  
 }
 
@@ -31,6 +33,16 @@ function isWinningCombination(first, second) {
 
 }
 
+
+function validatePlayerInput(playerChoice, player) {
+    const choices = ["spock", "lizard", "paper", "scissors", "rock"];
+    if (!choices.includes(playerChoice.toLowerCase())) {
+        throw new Error(`${player} - The choices must be either [${choices.join(", ")}], not <${playerChoice}>`);
+    }
+}
+
+
+// test
 console.log(rpsls('rock', 'lizard'))      // Player 1 Won!
 console.log(rpsls('paper', 'rock'))       // Player 1 Won!
 console.log(rpsls('scissors', 'lizard'))  // Player 1 Won!
